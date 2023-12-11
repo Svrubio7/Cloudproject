a874fdd932e605706c7c247f6f0d00cbe2a9b615
BaptisteEtroy <149064326+BaptisteEtroy@users.noreply.github.com>
2023-12-11 18:43:20 +0100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,49 +1,49 @@
-from flask import Flask, request
-import threading
-import datetime
+from flask import Flask, request, jsonify, render_template
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
 
 app = Flask(__name__)
+scheduler = BackgroundScheduler()
+scheduler.start()
 
-valve_status = {
-    'valve1': 'closed',
-    'valve2': 'closed',
-    'valve3': 'closed',
-    'valve4': 'closed'
-}
+def control_valve(valve_id, action):
+    print(f"Controlling {valve_id}: {action}")
+    # Add hardware control logic here
 
-def schedule_valve(valve, action, time_str):
-    time_format = "%H:%M"
-    target_time = datetime.datetime.strptime(time_str, time_format).time()
-    now = datetime.datetime.now().time()
+# New function to parse time and add job to the scheduler
+def schedule_valve_operation(valve_id, action, time_str):
+    time = datetime.strptime(time_str, '%H:%M').time()
+    scheduler.add_job(
+        func=control_valve,
+        trigger='cron',
+        hour=time.hour,
+        minute=time.minute,
+        args=[valve_id, action],
+        replace_existing=True
+    )
+    print(f"Scheduled {valve_id} to {action} at {time_str}")
 
-    # Calculate time difference
-    delta_seconds = (datetime.datetime.combine(datetime.date.today(), target_time) - 
-                     datetime.datetime.combine(datetime.date.today(), now)).seconds
+@app.route('/')
+def index():
+    return render_template('frontend.html')
 
-    if delta_seconds < 0:
-        # If time is past, schedule for the next day
-        delta_seconds += 86400  # seconds in a day
-
-    threading.Timer(delta_seconds, change_valve_status, [valve, action]).start()
-
-def change_valve_status(valve, action):
-    global valve_status
-    valve_status[valve] = action
-    print(f"Valve {valve} is now {action}")
-
-@app.route('/control', methods=['POST'])
-def control():
+@app.route('/api/set-time', methods=['POST'])
+def set_time():
     data = request.json
-    valve = data['valve']
+    valve_id = data['deviceId']
     action = data['action']
-    time = data.get('time')
+    time = data['time']
+    schedule_valve_operation(valve_id, action, time)
+    return jsonify(success=True)
 
-    if time:
-        schedule_valve(valve, action, time)
-    else:
-        change_valve_status(valve, action)
+# New endpoint to handle immediate valve status changes
+@app.route('/api/change-valve-status', methods=['POST'])
+def change_valve_status():
+    data = request.json
+    valve_id = data['valveId']
+    action = data['action']
+    control_valve(valve_id, action)  # Call the control function directly
+    return jsonify(success=True)
 
-    return f"Request to {action} {valve} received."
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
